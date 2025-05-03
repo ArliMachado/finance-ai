c3ecc85e8119c2dceef27c3676fb3b39a1da3f9d
Arli Machado <arli.machado@gmail.com>
2025-05-02 23:41:09 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <h1>Hello world</h1>
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="p-5 text-red-500">Hello world</h1>
+    </div>
   );
 }
